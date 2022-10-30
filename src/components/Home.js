460781aa8754bed85ea0a32a7e059b8c5f141a52
alankriti-mallick <alankriti1108@gmail.com>
2022-10-30 19:24:07 +0530

    home not done yet

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import homeBg from "../assets/images/home-bg.png";
 
 // use-ref - https://the-goonies.webflow.io/
 
 const Home = () => {
+  useEffect(() => {
+    // const handleScroll = event => {
+    //   // console.log("window.scrollY", window.scrollY);
+    //   var scroll = event.currentTarget.scrollTop;
+    //   //document.getElementById("#parallex");
+
+    // window.addEventListener("scroll", handleScroll);
+
+    // return () => {
+    //   window.removeEventListener("scroll", handleScroll);
+    // };
+  }, []);
+
   return (
     <section className="min-h-screen w-full bg-red-400 text-white flex justify-center items-center">
-      <h1>Hello CSE-UNITED</h1>
+      <img src={homeBg} alt="home-bg" className="z-10 w-full" />
+      {/* <h1 className="z-0">Hello CSE-UNITED</h1> */}
     </section>
   );
 };
