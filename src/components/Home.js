a0ceac38a72600b717bbd55c5ebf3aee47057029
alankriti-mallick <alankriti1108@gmail.com>
2022-10-31 22:48:59 +0530

    some zoom done but not working properly yet

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useRef } from "react";
 import homeBg from "../assets/images/home-bg.png";
 
 // use-ref - https://the-goonies.webflow.io/
 
 const Home = () => {
+  const ref = useRef(null);
+
   useEffect(() => {
-    // const handleScroll = event => {
-    //   // console.log("window.scrollY", window.scrollY);
-    //   var scroll = event.currentTarget.scrollTop;
-    //   //document.getElementById("#parallex");
 
-    // window.addEventListener("scroll", handleScroll);
+    const ele = ref.current;
+    const handleScroll = (event) => {
+      console.log("window.scrollY", window.scrollY);
+      var scroll = event.currentTarget.scrollTop;
+      ele.style.transform = 'scale(5,5)';
+    };
 
-    // return () => {
-    //   window.removeEventListener("scroll", handleScroll);
-    // };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   return (
     <section className="min-h-screen w-full bg-red-400 text-white flex justify-center items-center">
-      <img src={homeBg} alt="home-bg" className="z-10 w-full" />
+      <img src={homeBg} alt="home-bg" className="z-10 w-full" id="parallex" ref={ref}/>
       {/* <h1 className="z-0">Hello CSE-UNITED</h1> */}
     </section>
   );
