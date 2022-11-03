80a15bb863958022c96b9a38f4ded6b6035d1e7f
alankriti-mallick <alankriti1108@gmail.com>
2022-11-03 21:25:47 +0530

    contact form basic 2, responsiveness not yet added, more features to add

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Iframe from "react-iframe";
+
+// ref: https://codepen.io/krisantuswanandi/pen/KxrgeZ?editors=1100
 
 const Contact = () => {
   return (
     <section className="min-h-[90vh] w-full bg-blue-300 text-white flex justify-evenly items-center">
       <div className="background flex min-h-[100vh] items-center">
-        <div className="form-container p-[10px] w-[500px]">
-          <div className="form relative bg-slate-800 rounded-xl">
-            <div className="form-header flex items-center py-[10px] px-[20px] bg-slate-500 rounded-t-xl h-8">
+        <div className="form-container p-[10px] w-[1000px] h-[600px] flex items-center justify-center">
+          <div className="form bg-slate-800 rounded-xl w-full h-full">
+            <div className="form-header flex items-center py-[10px] px-[20px] bg-slate-500 rounded-t-xl h-12">
               <div className="form-header-left w-1/2 flex justify-left">
                 <div className="form-header-button close mx-1 inline-block w-2 h-2 mr-1 rounded-full bg-rose-600"></div>
                 <div className="form-header-button maximize mx-1 inline-block w-2 h-2 mr-1 rounded-full bg-amber-300"></div>
@@ -18,33 +21,57 @@ const Contact = () => {
                 <div className="form-header-ellipsis w-1 h-1 ml-1 rounded-full bg-slate-400"></div>
               </div>
             </div>
-            <div className="form-body flex">
-              <div className="form-body-item left p-14 flex flex-col">
-                <div className="app-title flex flex-col relative text-xl">
+            <div className="form-body flex h-[90%] p-14">
+              <div className="form-body-item left flex flex-col justify-between w-1/2">
+                <div className="app-title flex flex-col text-4xl">
                   <span>CONTACT</span>
                   <span>US</span>
                 </div>
-                <div className="app-contact text-[8px]">
+                <div className="form-map rounded-lg">
+                  <Iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.078405559548!2d88.42484061488224!3d22.57617068518093!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a02751a9d9c9e85%3A0x7fe665c781b10383!2sTechno%20Main%20Salt%20Lake!5e0!3m2!1sen!2sin!4v1667490398485!5m2!1sen!2sin"
+                    width="300"
+                    height="200"
+                    style="border:0;"
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                    className="rounded-lg"
+                  />
+                </div>
+                <div className="app-contact text-sm justify-end">
                   CONTACT INFO : +62 81 314 928 595
                 </div>
               </div>
-              <div className="form-body-item">
-                <div className="app-form">
+              <div className="form-body-item w-1/2 flex flex-col justify-center ">
+                <div className="app-form flex h-full w-full justify-evenly flex-col">
                   <div className="app-form-group">
-                    <input className="app-form-control" placeholder="NAME" />
+                    <input
+                      className="app-form-control w-full h-8 px-1 bg-transparent"
+                      placeholder="NAME"
+                    />
                   </div>
                   <div className="app-form-group">
-                    <input className="app-form-control" placeholder="EMAIL" />
+                    <input
+                      className="app-form-control w-full h-8 px-1 bg-transparent"
+                      placeholder="EMAIL"
+                    />
                   </div>
                   <div className="app-form-group">
-                    <input className="app-form-control" placeholder="CONTACT NO" />
+                    <input
+                      className="app-form-control w-full h-8 px-1 bg-transparent"
+                      placeholder="CONTACT NO"
+                    />
                   </div>
                   <div className="app-form-group message">
-                    <input className="app-form-control" placeholder="MESSAGE" />
+                    <textarea
+                      className="app-form-control w-full h-12 px-1 bg-transparent"
+                      placeholder="MESSAGE"
+                    />
                   </div>
-                  <div className="app-form-group buttons">
-                    <button className="app-form-button">CANCEL</button>
-                    <button className="app-form-button">SEND</button>
+                  <div className="app-form-group buttons flex justify-evenly">
+                    <button className="app-form-button p-2 rounded-lg bg-slate-400">CANCEL</button>
+                    <button className="app-form-button p-2 rounded-lg bg-slate-400">SEND</button>
                   </div>
                 </div>
               </div>
