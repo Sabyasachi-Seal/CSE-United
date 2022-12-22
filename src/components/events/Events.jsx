065e6ad63abd25bfef4c26696f73e0bcc17c0169
Sabyasachi <yoboy907@gmail.com>
2022-12-22 22:53:45 +0530

    Added Events

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -1,24 +1,198 @@
-import React from 'react';
+import React from "react";
+import {
+  Alert,
+  CCard,
+  CCardImage,
+  CCardBody,
+  CCardTitle,
+  CCardText,
+  CButton,
+  CCardLink,
+  CCardFooter,
+  CRow,
+  CCol,
+  CCardSubtitle,
+} from "@coreui/react";
+import "@coreui/coreui/dist/css/coreui.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Events = () => {
   return (
     <section
-      id='events'
-      className='min-h-[80vh] w-full bg-transparent text-white flex flex-col items-center pt-16 pl-16'
+      id="events"
+      className="min-h-[80vh] w-full bg-transparent text-white flex flex-col items-center pt-16 pl-16"
     >
-      <h1 className='text-6xl font-heading shadow-heading border-b-2 border-red-600'>
-        Ev<span className='text-red-500'>ent</span>s
+      <h1 className="text-6xl font-heading shadow-heading border-b-2 border-red-600">
+        Ev<span className="text-red-500">ent</span>s
       </h1>
-      <div className='pt-12'>
-        <p
-          className='text-6xl font-heading italic'
-          data-aos='fade-right'
-          data-aos-offset='100'
-          data-aos-easing='ease-in-sine'
-          data-aos-once='true'
-        >
-          Coming Soon
-        </p>
+      <div className="pt-12">
+        <CRow xs={{ cols: 1, gutter: 4 }} md={{ cols: 2 }}>
+          <CCol xs>
+            <CCard
+              className="mb-3"
+              style={{
+                maxHeight: "50rem",
+                maxWidth: "50rem",
+                backgroundColor: "#2B2D2F",
+              }}
+            >
+              <CRow className="g-0">
+                <CCol md={4}>
+                  <CCardImage
+                    style={{ maxHeight: "50rem", maxWidth: "50rem" }}
+                    src="/mnmcse.jpeg"
+                  />
+                </CCol>
+                <CCol md={8}>
+                  <CCardBody style={{ color: "#990033" }}>
+                    <CCardTitle className="text-center fs-1">
+                      Mr and Mrs CSE
+                    </CCardTitle>
+                    <CCardText>
+                      Are you ready to showcase your best and bring out the
+                      talent in you? Audition for Mr. And Ms. CSE and get a
+                      chance to win the department's most prestigious titles.
+                      So, what are you waiting for?! Only CSE, CST, and CSBS
+                      students from 1st and 2nd Years can apply
+                    </CCardText>
+                    <CButton
+                      className="center"
+                      color="danger"
+                      variant="outline"
+                      href="https://forms.gle/DtTF6czUYrn2qKfA6" target="_blank"
+                    >
+                      Register
+                    </CButton>
+                  </CCardBody>
+                </CCol>
+              </CRow>
+            </CCard>
+          </CCol>
+          <CCol xs>
+            <CCard
+              className="mb-3"
+              style={{
+                maxHeight: "50rem",
+                maxWidth: "50rem",
+                backgroundColor: "#2B2D2F",
+              }}
+            >
+              <CRow className="g-0">
+                <CCol md={4}>
+                  <CCardImage
+                    style={{ maxHeight: "50rem", maxWidth: "50rem" }}
+                    src="/batminton.jpeg"
+                  />
+                </CCol>
+                <CCol md={8}>
+                  <CCardBody style={{ color: "#990033" }}>
+                    <CCardTitle className="text-center fs-1">
+                      Batminton
+                    </CCardTitle>
+                    <CCardText>
+                      Hey there Shuttlers! Ready to test your badminton skills?
+                      Then register for the most compelling Badminton Tournament
+                      for free! Here is your chance to prove your talents and
+                      win Exciting goodies!
+                    </CCardText>
+                    <CButton
+                      className="center"
+                      color="danger"
+                      variant="outline"
+                      href="https://forms.gle/Kd31XKVxwj9Qa6cx8" target="_blank"
+                    >
+                      Register
+                    </CButton>
+                  </CCardBody>
+                </CCol>
+              </CRow>
+            </CCard>
+          </CCol>
+          <CCol xs>
+            <CCard
+              className="mb-3"
+              style={{
+                maxHeight: "50rem",
+                maxWidth: "50rem",
+                backgroundColor: "#2B2D2F",
+              }}
+            >
+              <CRow className="g-0">
+                <CCol md={4}>
+                  <CCardImage
+                    style={{ maxHeight: "50rem", maxWidth: "50rem" }}
+                    src="/valo.jpeg"
+                  />
+                </CCol>
+                <CCol md={8}>
+                  <CCardBody style={{ color: "#990033" }}>
+                    <CCardTitle className="text-center fs-1">
+                      Valorant
+                    </CCardTitle>
+                    <CCardText>
+                      Do you want to get an invite to the ultimate Valorant
+                      battle? Register for the most-exciting gaming tournament
+                      and compete from the comfort of your home! Book your spot
+                      today and get a chance to win prizes from a prize pool of
+                      Rs. 2000. P.S.: We have limited slots for 16 teams. So,
+                      what are you waiting for?!
+                    </CCardText>
+                    <CButton
+                      className="center"
+                      color="danger"
+                      variant="outline"
+                      href="https://forms.gle/PRupLCGzXdZdfjP66" target="_blank"
+                    >
+                      Register
+                    </CButton>
+                  </CCardBody>
+                </CCol>
+              </CRow>
+            </CCard>
+          </CCol>
+          <CCol xs>
+            <CCard
+              className="mb-3"
+              style={{
+                maxHeight: "50rem",
+                maxWidth: "50rem",
+                backgroundColor: "#2B2D2F",
+              }}
+            >
+              <CRow className="g-0">
+                <CCol md={4}>
+                  <CCardImage
+                    style={{ maxHeight: "50rem", maxWidth: "50rem" }}
+                    src="/omtf.jpeg"
+                  />
+                </CCol>
+                <CCol md={8}>
+                  <CCardBody style={{ color: "#990033" }}>
+                    <CCardTitle className="text-center fs-1">
+                      One Minute to Fame
+                    </CCardTitle>
+                    <CCardText>
+                      Still waiting for a chance to showcase your unique talents
+                      in front of the world? CSE UNITED presents 1 minute to
+                      Fame, an awesome platform to display your talents in front
+                      of your everyone. Participants will be given exactly a
+                      minute to perform anything of their choice in front of the
+                      judges.
+                    </CCardText>
+                    <CButton
+                      className="center"
+                      color="danger"
+                      variant="outline"
+                      href="https://forms.gle/dGAR5XYWdjKBAecr9" target="_blank"
+                    >
+                      Register
+                    </CButton>
+                  </CCardBody>
+                </CCol>
+              </CRow>
+            </CCard>
+          </CCol>
+        </CRow>
       </div>
     </section>
   );
