a689b37d0318a847c0e3fb2fc5bf0fe1db4b4465
Sabyasachi <yoboy907@gmail.com>
2023-01-05 20:07:13 +0530

    Added 4 sponsors

--- src/components/sponsors/Sponsors.jsx
+++ src/components/sponsors/Sponsors.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+// import React, { Component } from "react";
+// import ReactDOM from "react-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
+// import { Carousel } from "react-responsive-carousel";
 
 const Sponsors = () => {
   return (
     <section
       id="special-events"
-      className="w-full flex flex-col items-center pt-10 pb-10 text-white"
+      className="bg-transparent backdrop-blur-md pl-10 pr-10 w-full flex flex-col items-center pt-10 pb-10 text-white"
     >
       <h1 className="text-6xl font-heading shadow-heading border-b-2 border-red-600 mb-8">
         <span>
@@ -22,25 +22,25 @@ const Sponsors = () => {
           Coming<span className="text-red-500 font-heading"> Soon</span>...
         </span>{" "}
       </h5> */}
-      <div class="grid grid-cols-3 gap-4">
+      <div class="grid grid-cols-3 gap-6">
         <div className="rounded-md">
-          <a href=""><img class="rounded-full" src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGljfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" /></a>
+          <a href="https://prestogifts.com/mobile/"><img class="" src="https://www.smergers.com/media/franchiselogos/Presto_LOGO.png" alt=""/></a>
         </div>
         <div className="rounded-md">
-          <a href=""><img class="rounded-full" src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGljfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" /></a>
+          <a href="https://www.decathlon.in/"><img class="" src="https://logos-world.net/wp-content/uploads/2020/12/Decathlon-Logo.png" alt=""/></a>
         </div>
         <div className="rounded-md">
-          <a href=""><img class="rounded-full" src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGljfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" /></a>
+          <a href="https://ayuhealthandwellness.com/wp/"><img class="" src="https://cdn.discordapp.com/attachments/1036282124468244572/1059881867416973382/Bottle.png" alt=""/></a>
         </div>
         <div className="rounded-md">
-          <a href=""><img class="rounded-full" src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGljfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" /></a>
+          <a href="https://kwikpic.in/"><img class="" src="https://www.kwikpic.in/blog/wp-content/uploads/2022/10/Primary-Logo-e1667194420295-2048x532.png" alt=""/></a>
+        </div>
+        {/* <div className="rounded-md">
+          <a href=""><img class="" src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGljfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt=""/></a>
         </div>
         <div className="rounded-md">
-          <a href=""><img class="rounded-full" src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGljfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" /></a>
-        </div>
-        <div className="rounded-md">
-          <a href=""><img class="rounded-full" src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGljfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" /></a>
-        </div>
+          <a href=""><img class="" src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGljfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt=""/></a>
+        </div> */}
       </div>
     </section>
   );
